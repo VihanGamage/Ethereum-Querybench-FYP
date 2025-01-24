9b0403321d0a9d00f8c1e1d8d116533bb3bb4788
VihanGamage <vihanchandula@gmail.com>
2025-01-24 23:08:38 +0530

    simple find queries added

--- src/app/api/query/route.js
+++ src/app/api/query/route.js
@@ -13,31 +13,49 @@ export async function GET(req) {
     //query
     let returnData;
 
-    if (query===".find()"){    //all blocks
+    if (query==="find()"){    //all blocks
         const data = await
             offChainDataCollection.find().toArray();
         console.log(data)
         returnData=data;
 
-    }else if(query==="Tom"){     //specific blocks
+    }else if(query.includes("find({Name")) {
+
+        const nameValue = query.split(":")[1].trim().slice(0, -2);
         const data = await
-            offChainDataCollection.find({Name:query}).toArray();
+            offChainDataCollection.find({Name: nameValue}).toArray();
         console.log(data)
-        returnData=data;
+        returnData = data;
 
-    }else {
-
-        // Split the query string by commas and trim any whitespace
-        const hashCodes = query.split(",").map(hash => hash.trim());
-        console.log("Parsed Hash Codes:", hashCodes);
-
+    }else if(query.includes("find({Surname")) {
+        const surnameValue = query.split(":")[1].trim().slice(0, -2);
         const data = await
-            offChainDataCollection.find({hashCode: {$in: hashCodes}}).toArray();
+            offChainDataCollection.find({Surname: surnameValue}).toArray();
+        console.log(data)
+        returnData = data;
 
-        console.log(data);
-        returnData=data;
+    }else if(query.includes("find({Gender")) {
+        const genderValue = query.split(":")[1].trim().slice(0, -2);
+        const data = await
+            offChainDataCollection.find({Gender: genderValue}).toArray();
+        console.log(data)
+        returnData = data;
+
+    }else if(query.includes("find({Occupation")) {
+        const occupationValue = query.split(":")[1].trim().slice(0, -2);
+        const data = await
+            offChainDataCollection.find({Occupation: occupationValue}).toArray();
+        console.log(data)
+        returnData = data;
+
+    }else if(query.includes("find({hashCode")) {
+        const hashCodeValue = query.split(":")[1].trim().slice(0, -2);
+        const data = await
+            offChainDataCollection.find({hashCode: hashCodeValue}).toArray();
+        console.log(data)
+        returnData = data;
+
     }
-
 
     //----------------------//
     return new Response(JSON.stringify(returnData), {
