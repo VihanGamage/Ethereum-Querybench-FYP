3324bd6a058f2a6c996697d6242aecb9ede3ee2b
VihanGamage <vihanchandula@gmail.com>
2025-02-16 15:41:32 +0530

    header link added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,17 @@
 import Image from "next/image";
 import {Label} from "@/components/ui/label";
+import Link from "next/link";
 
 export default function Header() {
 
     return(
 
-        <div className="flex items-center justify-center mt-8">
+        <Link href="/" className="flex items-center justify-center mt-8">
             <Image src="/logo.webp" alt="logo" height={60} width={40}/>
             <Label className="text-3xl ml-2">
                 Ethereum QueryBench
             </Label>
-        </div>
+        </Link>
     )
 
 }
